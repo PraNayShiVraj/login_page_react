180683e6431670b25ec0131527b9a4999a2ebf3d
pranay <pranayshivraj13@gmail.com>
2026-04-17 22:06:37 +0530

    signup issue fix

--- frontend/my-react-app/src/Signup.jsx
+++ frontend/my-react-app/src/Signup.jsx
@@ -18,7 +18,7 @@ function Signup() {
         e.preventDefault();
 
         try {
-            const res = await fetch("http://127.0.0.1:8000/signup", {
+            const res = await fetch("https://login-page-react.onrender.com/signup", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
